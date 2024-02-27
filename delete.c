2cd9fd862edabffaa182a0379352937534c2be1e
srilakshmi2006 <161099863+srilakshmi2006@users.noreply.github.com>
2024-02-27 11:26:56 +0530

    Add files via upload

---
+++ delete.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a[5]={1,2,3,4,5};
+	int pos=2,i,size=5;
+	for(i=pos-1;i<5;i++)
+	a[i]=a[i+1];
+	size--;
+	for(i=0;i<size;i++)
+	printf("%d",a[i]);
+	return 0;
+}
