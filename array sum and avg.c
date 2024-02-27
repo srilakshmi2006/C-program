28a34b5480af54441057b02f872b3b5e561c9257
srilakshmi2006 <161099863+srilakshmi2006@users.noreply.github.com>
2024-02-27 11:26:05 +0530

    Add files via upload

---
+++ array sum and avg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	 int a[5]={20,40,60,80,100};
+	 int sum=0,i;
+	 float avg;
+	 for(i=0;i<5;i++)
+	 {
+	 	sum=sum+a[i];
+	 }
+	 avg=sum+a[i];
+	 printf("%d\n",sum);
+	 printf("%f",avg);
+	 return 0;
+	
+}
