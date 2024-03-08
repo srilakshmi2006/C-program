3555b9525b9d3051c4deab713b3cc4bcaeee2748
srilakshmi2006 <161099863+srilakshmi2006@users.noreply.github.com>
2024-03-08 14:14:06 +0530

    Update div arg.c

--- div arg.c
+++ div arg.c
@@ -14,6 +14,18 @@ void mul()
 	mul=a*b;
 	printf("multiplication =%d\n",mul);
 }
+void mod()
+{
+	int a=10,b=20,mod;
+	mod=a%b;
+	printf("modulus =%d\n",mod);
+}
+void sum()
+{
+          int a=10,b=20,sum;
+          sum=a+b;
+          printf("addition=%d\n",sum);
+}
 
 {
 	div();
